d1c840c35a64a04d68eca8d3e9824058b1283aed
franck <franck.kone@mykenzie.snhu.edu>
2023-05-16 00:56:41 -0400

    completed assessment oojs methods practice weeks 17 matching both results displaying

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 const coin = {
   state: 0,
+  result: [], // to save the result of display20Flips function
   flip: function () {
     // 1. Randomly set your coin object's "state" property to be either
     //    0 or 1: use "this.state" to access the "state" property on this object.
     let stateVariable = Math.round(Math.random());
     this.state += stateVariable
-    return this.state
   },
   toString: function () {
     // 2. Return the string "Heads" or "Tails", depending on whether
@@ -37,14 +37,6 @@ function display20Flips() {
   // to clean the body
   document.body.innerHTML = ''
 
-  //create HTML elements necessary for the displaying 
-  let gameContainer = document.createElement('div')
-  gameContainer.setAttribute('id', 'gameBox')
-
-  let resultDiv = document.createElement('div')
-  resultDiv.setAttribute('id', 'resultsTextBox')
-
-
   // 4. Use a loop to flip the coin 20 times, each time displaying the result of the flip as a string on the page.  After your loop completes, return an array with the result of each flip.
   for (let flipTimes = 0 ; flipTimes < 20; flipTimes++){
 
@@ -52,42 +44,57 @@ function display20Flips() {
     coin.flip()
     results.push(coin.toString())
 
-    // displaying each result to the page as a string 
-    let resultContent = document.createElement('span')
-    resultContent.innerText = coin.toString()
-    resultDiv.append(resultContent)
-    gameContainer.append(resultDiv)
-    document.body.append(gameContainer)
-
     // reset the state property before another flip's turn
     coin.state = 0
   }
+  coin.result = results
   return results
 }
-display20Flips()
+
+// display the result of dosphay20Flips function
+let gameContainer = document.createElement('div')
+gameContainer.setAttribute('id', 'gameBox')
+
+let resultDiv = document.createElement('div')
+resultDiv.setAttribute('id', 'resultsTextBox')
+
+resultDiv.innerText = JSON.stringify(display20Flips())
+
+gameContainer.append(resultDiv)
+document.body.append(gameContainer)
 
 function display20Images() {
   const results = [];
-  let gameContainer = document.querySelector('div')
 
-  let resultDiv = document.createElement('div')
-  resultDiv.setAttribute('id', 'imagesBox')
+  // to match the result of display20Flips function
+  let flipsResult = coin.result
 
   // 5. Use a loop to flip the coin 20 times, and display the results of each flip as an image on the page.  After your loop completes, return an array with result of each flip.
-  for (let flipTimes = 0; flipTimes < 20; flipTimes++) {
+  for (let flipTimes = 0; flipTimes < flipsResult.length; flipTimes++) {
 
-    // set the result of each coin flipped to the results array
-    coin.flip()
-    results.push(coin.toString())
-    let image = coin.toHTML()
-
-    // displaying each result to the page as a string 
-    resultDiv.append(image)
-    gameContainer.append(resultDiv)
-
-    // reset the state property before another flip's turn
-    coin.state = 0
+    if(flipsResult[flipTimes] === 'Heads'){
+      coin.state = 1;
+      coin.toString()
+      results.push(coin.toHTML())
+    
+    } else if (flipsResult[flipTimes] === 'Tails'){
+      coin.state = 0;
+      coin.toString()
+      results.push(coin.toHTML())
+     
+    }
+    
   }
   return results
 }
-display20Images()
+
+// showing up the result of display20Images matching with display20Flips
+
+let imagesDiv = document.createElement('div')
+imagesDiv.setAttribute('id', 'imagesBox')
+
+display20Images().forEach(image => {
+  imagesDiv.append(image)
+})
+
+gameContainer.append(imagesDiv)
